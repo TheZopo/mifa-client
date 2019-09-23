93f4c726063ec4fe5aa525f374d8398d30e9668b
Sadikk <youpi.killeur@gmail.com>
2019-09-23 18:14:45 +0200

    [feat] wip - basic cli client

--- src/main/java/fr/mifa/client/MIFAClient.java
+++ src/main/java/fr/mifa/client/MIFAClient.java
@@ -1,7 +1,31 @@
 package fr.mifa.client;
 
+import fr.mifa.core.models.TextMessage;
+import fr.mifa.core.network.Client;
+import fr.mifa.core.network.protocol.MessagePacket;
+
+import java.util.Scanner;
+
 public class MIFAClient {
     public static void main(String[] args) {
         System.out.println("Hello World mifa-client !");
+
+        Client client = new Client();
+        client.connect("localhost", 2021);
+
+        try ( Scanner scanner = new Scanner( System.in ) ) {
+            while( true ) {
+                System.out.print( "> " );
+                String text = scanner.nextLine();
+
+                TextMessage message = new TextMessage();
+                message.setText(text);
+                message.setRoomId(1);
+
+                MessagePacket packet = new MessagePacket();
+                packet.setMessage(message);
+                client.send(packet);
+            }
+        }
     }
 }
