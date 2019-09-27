2c9e9c58ea7885b79237fcb33fdc4abe49fff321
Sadikk <youpi.killeur@gmail.com>
2019-09-27 20:13:52 +0200

    [feat] handling disconnection packet

--- src/main/java/fr/mifa/client/network/ClientPacketManager.java
+++ src/main/java/fr/mifa/client/network/ClientPacketManager.java
@@ -1,16 +1,11 @@
 package fr.mifa.client.network;
 
 import fr.mifa.client.services.RoomService;
+import fr.mifa.core.network.protocol.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import fr.mifa.core.network.PacketManager;
-import fr.mifa.core.network.protocol.HelloWorldPacket;
-import fr.mifa.core.network.protocol.JoinedRoomPacket;
-import fr.mifa.core.network.protocol.LeftRoomPacket;
-import fr.mifa.core.network.protocol.MessageSentPacket;
-import fr.mifa.core.network.protocol.Packet;
-import fr.mifa.core.network.protocol.RoomListPacket;
 
 public class ClientPacketManager extends PacketManager {
     private static final Logger logger = LoggerFactory.getLogger(ClientPacketManager.class);
@@ -37,6 +32,11 @@ public class ClientPacketManager extends PacketManager {
             // TODO alert user
             logger.debug("Received HelloWorldPacket");
             logger.info("Successfully connected to the server !");
+        }
+        else if(packet instanceof DisconnectPacket) {
+            logger.warn("Received DisconnectPacket");
+            // TODO alert user
+            System.exit(0);
         }
         else {
             logger.warn("Received unhandled packet : " + packet.getClass().getName());
